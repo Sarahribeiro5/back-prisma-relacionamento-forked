85662fc1d0e41b5e23dccae4e20fd3275fc36261
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-04-29 09:34:45 -0300

    feat: create user routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import UserModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
 class AuthController {
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import AuthController from '../controllers/authcontroller.js';
+
+const authRouter = express.Router();
+
+authRouter.post("/register", AuthController.register);
+
+export default authRouter;
