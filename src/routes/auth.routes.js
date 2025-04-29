22f69235862e06f54519a4205abe1db27ab5ccf8
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-04-29 11:01:38 -0300

    feat: create login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import AuthController from '../controllers/authcontroller.js';
+import AuthController from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
 authRouter.post("/register", AuthController.register);
+authRouter.post("/login", AuthController.login);
 
 export default authRouter;
